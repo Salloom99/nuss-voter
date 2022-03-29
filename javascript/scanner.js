57552cbfc6975e55d664ad87421394548944480f
Salem Domani <salem.domani@gmail.com>
2022-03-29 15:11:45 +0300

    Improve scanner code readability.

--- javascript/scanner.js
+++ javascript/scanner.js
@@ -1,37 +1,51 @@
 function onScanSuccess(decodedText, decodedResult) {
-  // handle the scanned code as you like, for example:
-  // console.log(`Code matched = ${decodedText}`, decodedResult);
   desc.textContent = decodedText;
 }
 
 function onScanFailure(error) {
   let isScanning = scanner.stateManagerProxy.isScanning()
   console.log(isScanning);
-  // handle scan failure, usually better to ignore and keep scanning.
-  // for example:
   // console.warn(`Code scan error = ${error}`);
 }
 
 async function getCameraId() {
   var cameras = await Html5Qrcode.getCameras();
-  return cameras[0].id;
+  currentCameraId = cameras[currentCameraIndex].id;
 }
 
-let scanner = new Html5Qrcode(
-  "reader",
-  { fps: 10 },
-  /* verbose= */ false
-);
+function scannerIsReady() {
+  return scanner.stateManagerProxy.canScanFile();
+}
+
+function startScanner() {
+  scanner.start(currentCameraId, null, onScanSuccess, onScanFailure);
+}
+
+function stopScanner() {
+  scanner.stop();
+}
+
+function toggleIcon() {
+  cameraBtn.classList.toggle('fa-camera');
+  cameraBtn.classList.toggle('fa-qrcode');
+}
+
+function onCameraBtnClick() {
+  toggleIcon();
+  if (scannerIsReady())
+    startScanner();
+  else
+    stopScanner()
+}
+
+
+let scanner = new Html5Qrcode("reader", { fps: 10 }, false);
+
+let currentCameraIndex = 1;
+let currentCameraId = null;
 
 let cameraBtn = document.querySelector('.fa-camera');
 let desc = document.querySelector('.desc');
-cameraBtn.addEventListener('click',async ()=>{
-  var cameraId = await getCameraId();
-  cameraBtn.classList.toggle('fa-camera');
-  cameraBtn.classList.toggle('fa-qrcode');
-  if (scanner.stateManagerProxy.canScanFile())
-    scanner.start(cameraId, null, onScanSuccess, onScanFailure);
-  else
-    scanner.stop()
-});
+getCameraId();
+cameraBtn.addEventListener('click',onCameraBtnClick);
 
