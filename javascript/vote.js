9a72c235d936822973c7e03b65e64e0a86de47d6
Salem Domani <salem.domani@gmail.com>
2022-03-29 22:48:45 +0300

    Add count of votes.

--- javascript/vote.js
+++ javascript/vote.js
@@ -1,73 +1,75 @@
 var nominees = [
-    {name: 'سلوم حداد'},
-    {name: 'باسم ياخور'},
-    {name: 'أيمن زيدان'},
-    {name: 'عابد فهد'},
-    {name: 'جمال سليمان'},
-    {name: 'بسام طوسا'},
-    {name: 'خالد تاجا'},
-    {name: 'دريد لحام'},
-    {name: 'غسان مسعود'},
-    {name: 'عباس النوري'},
-    {name: 'ياسر العظمة'},
-    {name: 'أيمن رضا'},
-    {name: 'ماكسيم خليل'},
-    {name: 'قصي خولي'},
-    {name: 'أسعد فضة'},
-    {name: 'عبد الهادي الصباغ'},
-    {name: 'عبد الحكيم قطيفان'},
-    {name: 'تيم حسن'},
-    {name: 'يزن السيد'},
-    {name: 'سيف الدين سبيعي'},
-    {name: 'باسل خياط'},
-    {name: 'سليم صبري'},
+  { name: "سلوم حداد" },
+  { name: "باسم ياخور" },
+  { name: "أيمن زيدان" },
+  { name: "عابد فهد" },
+  { name: "جمال سليمان" },
+  { name: "بسام طوسا" },
+  { name: "خالد تاجا" },
+  { name: "دريد لحام" },
+  { name: "غسان مسعود" },
+  { name: "عباس النوري" },
+  { name: "ياسر العظمة" },
+  { name: "أيمن رضا" },
+  { name: "ماكسيم خليل" },
+  { name: "قصي خولي" },
+  { name: "أسعد فضة" },
+  { name: "عبد الهادي الصباغ" },
+  { name: "عبد الحكيم قطيفان" },
+  { name: "تيم حسن" },
+  { name: "يزن السيد" },
+  { name: "سيف الدين سبيعي" },
+  { name: "باسل خياط" },
+  { name: "سليم صبري" },
 ];
 
+function filterNominees(text) {
+  currentNominees.forEach((nominee) => {
+    if (nominee.name.includes(text)) nominee.element.classList.remove("hidden");
+    else nominee.element.classList.add("hidden");
+  });
+}
+
+function clearSearch() {
+  filterInput.value = "";
+  currentNominees.forEach((nominee) =>
+    nominee.element.classList.remove("hidden")
+  );
+}
 
 function createNominee(nominee) {
+  const card = nomineeCardTemplate.content.cloneNode(true).children[0];
 
-    const card = nomineeCardTemplate.content.cloneNode(true).children[0];
+  const name = card.querySelector(".manager__item__text");
 
-    const name = card.querySelector('.manager__item__text');
-    
-    name.textContent = nominee.name;
-    
-    const checkbox = card.querySelector('.checkbox');
-    
-    checkbox.onclick = ()=> {
-        nominee.checked = !nominee.checked;
-    };
-    
-    nomineesNode.prepend(card);
-    return card;
+  name.textContent = nominee.name;
+
+  const checkbox = card.querySelector(".checkbox");
+
+  checkbox.onclick = () => {
+    nominee.checked = !nominee.checked;
+    votes.textContent = `${nominees.filter(nominee=>nominee.checked).length}/12`;
+  };
+
+  nomineesNode.prepend(card);
+  return card;
 }
 
-const nomineeCardTemplate = document.querySelector('[data-nominee-template]')
+const nomineeCardTemplate = document.querySelector("[data-nominee-template]");
+const votes = document.getElementById('votes');
+const nomineesNode = document.querySelector(".manager__list");
+const filterInput = document.getElementById("filter-input");
 
-const nomineesNode = document.querySelector('.manager__list');
-const filterInput = document.getElementById('filter-input');
-document.querySelector('.manager__header__btn').addEventListener('click', ()=>{
-    filterInput.value = '';
-    currentNominees.forEach(nominee =>
-            nominee.element.classList.remove('hidden'));
-}
-)
 
-filterInput.oninput = (e)=> { 
-    currentNominees.forEach(nominee => {
-        if (nominee.name.includes(e.target.value))
-            nominee.element.classList.remove('hidden');
-        else
-            nominee.element.classList.add('hidden');
-    } );
-}
+document.getElementById('clear').addEventListener("click",clearSearch);
+filterInput.oninput = (e) => filterNominees(e.target.value);
 
 let currentNominees = nominees.map((nominee) => {
-    const nomineeCard = createNominee(nominee);
-    return {
-        name: nominee.name, 
-        checked: false,
-        hidden: false,
-        element: nomineeCard
-        };
+  const nomineeCard = createNominee(nominee);
+  return {
+    name: nominee.name,
+    checked: false,
+    hidden: false,
+    element: nomineeCard,
+  };
 });
