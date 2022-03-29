a8d40abcd03509c4ddb24f442f26a738faedc1a2
Salem Domani <salem.domani@gmail.com>
2022-03-29 21:13:11 +0300

    Refactor nominees html & css & js.

--- javascript/nominees.js
+++ javascript/nominees.js
@@ -1,100 +1,88 @@
 var nominees = [
-    {name: 'سلوم حداد', votes: 255},
-    {name: 'باسم ياخور', votes: 245},
-    {name: 'أيمن زيدان', votes: 230},
-    {name: 'عابد فهد', votes: 214},
-    {name: 'جمال سليمان', votes: 201},
-    {name: 'بسام طوسا', votes: 195},
-    {name: 'خالد تاجا', votes: 184},
-    {name: 'دريد لحام', votes: 177},
-    {name: 'غسان مسعود', votes: 162},
-    {name: 'عباس النوري', votes: 161},
-    {name: 'ياسر العظمة', votes: 156},
-    {name: 'أيمن رضا', votes: 153},
-    {name: 'ماكسيم خليل', votes: 147},
-    {name: 'قصي خولي', votes: 123},
-    {name: 'أسعد فضة', votes: 111},
-    {name: 'عبد الهادي الصباغ', votes: 94},
-    {name: 'عبد الحكيم قطيفان', votes: 80},
-    {name: 'تيم حسن', votes: 79},
-    {name: 'يزن السيد', votes: 50},
-    {name: 'سيف الدين سبيعي', votes: 33},
-    {name: 'باسل خياط', votes: 20},
-    {name: 'سليم صبري', votes: 11},
+  { name: "سلوم حداد" },
+  { name: "باسم ياخور" },
+  { name: "أيمن زيدان" },
+  { name: "عابد فهد" },
+  { name: "جمال سليمان" },
+  { name: "بسام طوسا" },
+  { name: "خالد تاجا" },
+  { name: "دريد لحام" },
+  { name: "غسان مسعود" },
+  { name: "عباس النوري" },
+  { name: "ياسر العظمة" },
+  { name: "أيمن رضا" },
+  { name: "ماكسيم خليل" },
+  { name: "قصي خولي" },
+  { name: "أسعد فضة" },
+  { name: "عبد الهادي الصباغ" },
+  { name: "عبد الحكيم قطيفان" },
+  { name: "تيم حسن" },
+  { name: "يزن السيد" },
+  { name: "سيف الدين سبيعي" },
+  { name: "باسل خياط" },
+  { name: "سليم صبري" },
 ];
 
-
-
 function handleDelete(event) {
-    event.target.parentElement.classList.toggle("delete");
-    setTimeout(()=>event.target.parentElement.remove(), 1000);
+  event.target.parentElement.classList.toggle("delete");
+  setTimeout(() => event.target.parentElement.remove(), 1000);
 }
 
 function handleEdit(event) {
-    var edit = event.target;
-    edit.classList.toggle('fa-edit');
-    edit.classList.toggle('fa-check');
-    var name = event.target.parentElement.querySelector('.name');
-    name.contentEditable = !name.isContentEditable;
-    name.focus();
+  var edit = event.target;
+  edit.classList.toggle("fa-edit");
+  edit.classList.toggle("fa-check");
+  var name = event.target.parentElement.querySelector(".name");
+  name.contentEditable = !name.isContentEditable;
+  name.focus();
 }
 
 function addNominee() {
-    var nomineeName = addNomineeInput.value;
-    addNomineeInput.value = '';
-    if (nomineeName === '' ||
-        nominees.some( nominee => nominee.name === nomineeName)) {
-            createSnackbar('لا يمكنك إضافة اسم فارغ');
-            return;
-        }
+  var nomineeName = addNomineeInput.value;
+  addNomineeInput.value = "";
+  if (
+    nomineeName === "" ||
+    nominees.some((nominee) => nominee.name === nomineeName)
+  ) {
+    createSnackbar("لا يمكنك إضافة اسم فارغ");
+    return;
+  }
 
-    nominees.push({name: nomineeName, count: 0});
-    var name = createNominee();
-    name.innerText = nomineeName;
-    
+  nominees.push({ name: nomineeName, count: 0 });
+  createNominee({name: nomineeName});
 }
 
-var addNomineeInput = document.querySelector('.add-nominee .input');
-var nomineesNode = document.querySelector('.nominees');
+function createNominee(nominee) {
+  const card = nomineeCardTemplate.content.cloneNode(true).children[0];
 
-document.querySelector('.fa-circle-plus').onclick = addNominee;
-document.querySelector('.menu__btn').onclick = () => { window.location.href = '/dashboard.html'};
-document.querySelector('.input').addEventListener('keyup', event => {
-    if (event.key === 'Enter')
-        addNominee();
-})
-    
+  const name = card.querySelector(".manager__item__text");
+  name.textContent = nominee.name;
 
-function createNominee() {
-    var name = document.createElement('span');
-    name.className = 'name';
-    
-    var editIcon = document.createElement('i');
-    editIcon.className = 'fa fa-solid fa-pen-to-square clickable';
-    editIcon.onclick = handleEdit;
-        
-    var trashIcon = document.createElement('i');
-    trashIcon.className = 'fa fa-solid fa-trash clickable';
-    trashIcon.onclick = handleDelete;
-        
-    var nomineeNode = document.createElement('li');
-    nomineeNode.className = 'nominee delete';
-        
-    nomineeNode.appendChild(name);
-    nomineeNode.appendChild(editIcon);
-    nomineeNode.appendChild(trashIcon);
-        
-    
-    nomineesNode.prepend(nomineeNode);
+  const editIcon = card.querySelector("#edit");
+  editIcon.onclick = handleEdit;
 
-    setTimeout(() => nomineeNode.classList.toggle('delete'), 10)
+  const trashIcon = card.querySelector("#delete");
+  trashIcon.onclick = handleDelete;
 
-    return name;
+  nomineesNode.prepend(card);
+
+  setTimeout(() => card.classList.toggle("delete"), 10);
 }
 
-nominees.forEach((nominee) => {
 
-    var name = createNominee();
-    name.innerText = nominee.name;
-    
+
+const nomineeCardTemplate = document.querySelector("[data-nominee-template]");
+
+var addNomineeInput = document.getElementById("addInput");
+const nomineesNode = document.querySelector("[data-nominees-container]");
+
+document.getElementById("add").onclick = addNominee;
+document.querySelector(".menu__btn").onclick = () => {
+  window.location.href = "/dashboard.html";
+};
+document.querySelector(".input").addEventListener("keyup", (event) => {
+  if (event.key === "Enter") addNominee();
 });
+
+
+nominees.forEach((nominee) => createNominee(nominee));
