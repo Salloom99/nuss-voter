ee9b59f9153d5a2a7ec1d216cef4277966de7515
Salem Domani <salem.domani@gmail.com>
2022-03-29 19:01:02 +0300

    Implement search bar for nominees.

--- javascript/vote.js
+++ javascript/vote.js
@@ -1,60 +1,73 @@
 var nominees = [
-    {name: 'سلوم حداد', votes: 255},
-    {name: 'باسم ياخور', votes: 245},
-    {name: 'أيمن زيدان', votes: 230},
-    {name: 'عابد فهد', votes: 214},
-    {name: 'جمال سليمان', votes: 201},
-    {name: 'بسام طوسا', votes: 195},
-    {name: 'خالد تاجا', votes: 184},
-    {name: 'دريد لحام', votes: 177},
-    {name: 'غسان مسعود', votes: 162},
-    {name: 'عباس النوري', votes: 161},
-    {name: 'ياسر العظمة', votes: 156},
-    {name: 'أيمن رضا', votes: 153},
-    {name: 'ماكسيم خليل', votes: 147},
-    {name: 'قصي خولي', votes: 123},
-    {name: 'أسعد فضة', votes: 111},
-    {name: 'عبد الهادي الصباغ', votes: 94},
-    {name: 'عبد الحكيم قطيفان', votes: 80},
-    {name: 'تيم حسن', votes: 79},
-    {name: 'يزن السيد', votes: 50},
-    {name: 'سيف الدين سبيعي', votes: 33},
-    {name: 'باسل خياط', votes: 20},
-    {name: 'سليم صبري', votes: 11},
+    {name: 'سلوم حداد'},
+    {name: 'باسم ياخور'},
+    {name: 'أيمن زيدان'},
+    {name: 'عابد فهد'},
+    {name: 'جمال سليمان'},
+    {name: 'بسام طوسا'},
+    {name: 'خالد تاجا'},
+    {name: 'دريد لحام'},
+    {name: 'غسان مسعود'},
+    {name: 'عباس النوري'},
+    {name: 'ياسر العظمة'},
+    {name: 'أيمن رضا'},
+    {name: 'ماكسيم خليل'},
+    {name: 'قصي خولي'},
+    {name: 'أسعد فضة'},
+    {name: 'عبد الهادي الصباغ'},
+    {name: 'عبد الحكيم قطيفان'},
+    {name: 'تيم حسن'},
+    {name: 'يزن السيد'},
+    {name: 'سيف الدين سبيعي'},
+    {name: 'باسل خياط'},
+    {name: 'سليم صبري'},
 ];
 
 
-function createNominee() {
-    // <li class="vote-card">
-    // <input class="checkbox" type="checkbox" name="" id="">
-    // <span class="name">
-    //     محمد سالم دوماني
-    // </span>
-    // </li>
-    var name = document.createElement('span');
-    name.className = 'name';
-    
-    var checkbox = document.createElement('input');
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.className = 'checkbox';
-        
-    var nomineeNode = document.createElement('li');
-    nomineeNode.className = 'vote-card';
-        
-    nomineeNode.appendChild(checkbox);
-    nomineeNode.appendChild(name);
-        
-    
-    nomineesNode.prepend(nomineeNode);
+function createNominee(nominee) {
 
-    return name;
+    const card = nomineeCardTemplate.content.cloneNode(true).children[0];
+
+    const name = card.querySelector('.name');
+    
+    name.textContent = nominee.name;
+    
+    const checkbox = card.querySelector('.checkbox');
+    
+    checkbox.onclick = ()=> {
+        nominee.checked = !nominee.checked;
+    };
+    
+    nomineesNode.prepend(card);
+    return card;
 }
 
-let nomineesNode = document.querySelector('.normal-list');
+const nomineeCardTemplate = document.querySelector('[data-nominee-template]')
 
-nominees.forEach((nominee) => {
+const nomineesNode = document.querySelector('.normal-list');
+const filterInput = document.getElementById('filter-input');
+document.querySelector('.fa-circle-xmark').addEventListener('click', ()=>{
+    filterInput.value = '';
+    currentNominees.forEach(nominee =>
+            nominee.element.classList.remove('hidden'));
+}
+)
 
-    var name = createNominee();
-    name.innerText = nominee.name;
-    
+filterInput.oninput = (e)=> { 
+    currentNominees.forEach(nominee => {
+        if (nominee.name.includes(e.target.value))
+            nominee.element.classList.remove('hidden');
+        else
+            nominee.element.classList.add('hidden');
+    } );
+}
+
+let currentNominees = nominees.map((nominee) => {
+    const nomineeCard = createNominee(nominee);
+    return {
+        name: nominee.name, 
+        checked: false,
+        hidden: false,
+        element: nomineeCard
+        };
 });
