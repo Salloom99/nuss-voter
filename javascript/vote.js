5af0cd58859a778e4acdb317ad81043cd863ba05
Salem Domani <salem.domani@gmail.com>
2022-03-30 00:04:12 +0300

    Validate form before vote.

--- javascript/vote.js
+++ javascript/vote.js
@@ -48,22 +48,41 @@ function createNominee(nominee) {
 
   checkbox.onclick = () => {
     nominee.checked = !nominee.checked;
-    votes.textContent = `${nominees.filter(nominee=>nominee.checked).length}/12`;
+    votesCount = nominees.filter((nominee) => nominee.checked).length;
+    votes.textContent = `${votesCount}/12`;
   };
 
   nomineesNode.prepend(card);
   return card;
 }
 
+function validateForm() {
+  if (votesCount > 12) {
+    createSnackbar(`لقد تجاوزت العدد المطلوب بـ ${votesCount - 12}`);
+    return false;
+  }
+  else if (votesCount < 12) {
+    createSnackbar(`تبقى لديك ${12 - votesCount} من الأصوات`);
+    return false;
+  }
+}
+
+
 const nomineeCardTemplate = document.querySelector("[data-nominee-template]");
-const votes = document.getElementById('votes');
+const votes = document.getElementById("votes");
 const nomineesNode = document.querySelector(".manager__list");
 const filterInput = document.getElementById("filter-input");
 
 
-document.getElementById('clear').addEventListener("click",clearSearch);
+document.querySelector('form').onsubmit = validateForm;
+document.getElementById("clear").addEventListener("click", clearSearch);
 filterInput.oninput = (e) => filterNominees(e.target.value);
+document.querySelector(".menu__btn").onclick = () => {
+  window.location.href = "/dashboard.html";
+};
 
+
+let votesCount = 0;
 let currentNominees = nominees.map((nominee) => {
   const nomineeCard = createNominee(nominee);
   return {
